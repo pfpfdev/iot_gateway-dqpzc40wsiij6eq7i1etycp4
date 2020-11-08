696b2243ef7780b1a9964829ae894853e7e97c05
pfpfdev <pfpfdev@gmail.com>
2020-11-08 11:48:03 +0900

    log系の実装

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"../internal"
+	"log"
+	"os"
+	"io"
 )
 
 func main(){
+	f,err:= os.OpenFile("/tmp/iot_gateway.log",os.O_APPEND|os.O_CREATE|os.O_WRONLY,0666)
+	if err != nil {
+		log.Fatal("Failed to set the log file")
+	}
+	defer f.Close()
+	log.SetOutput(io.MultiWriter(f,os.Stdout))
+
 	go internal.HttpServer()
 	internal.SocketServer()
 }
