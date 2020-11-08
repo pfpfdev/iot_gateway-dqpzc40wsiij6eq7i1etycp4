474a202491f301c4138448541a3ab9c3761830df
pfpfdev <pfpfdev@gmail.com>
2020-11-08 12:11:10 +0900

    deviceのテストファイルを用意

---
+++ test/device.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Device1"
+echo "ADD ope1"
+echo "REG ope1 LED OnOff"
+echo "REG ope1 speed Hundred"
+
+echo "ADD ope2"
+echo "REG ope2 power OnOff"
+
+echo "FIN"
+
+while true
+do
+    echo ""
+    sleep 3
+done
